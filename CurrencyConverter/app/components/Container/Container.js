5d9a0333f4ef2b4e2c4bbfa293c7f61fb2cb4fdf
jstanas <35541156+jstanas@users.noreply.github.com>
2018-08-13 21:05:49 -0500

    Add files via upload

---
+++ CurrencyConverter/app/components/Container/Container.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './styles';
+
+import { View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+
+const Container = ({ children, backgroundColor }) => {
+  const containerStyles = [styles.container];
+  if (backgroundColor) {
+    containerStyles.push({ backgroundColor });
+  }
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={containerStyles}>
+        {children}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+Container.propTypes = {
+    children: PropTypes.any,
+    backgroundColor: PropTypes.string,
+};
+
+export default Container;
